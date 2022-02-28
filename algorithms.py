f8886149343ff13c245f67d1f3e0d7f7afb836e6
frequensy <mametevalex@gmail.com>
2022-02-28 15:28:26 +0300

    Calculate best measurments time

--- algorithms.py
+++ algorithms.py
@@ -5,7 +5,6 @@ from numpy import pi
 from flux_bias import FluxBiasController
 from qubit import Qubit
 from utils import h, i, rotate_gate, default_mu, adamar_gate
-from random import uniform
 
 
 class Algorith:
@@ -13,35 +12,30 @@ class Algorith:
         f_min, f_max = field_range
         self.f_min, self.f_max = f_min, f_max
         self.n = n
-        self.iters= measurements_iterations
+        self.iters = measurements_iterations
         self.m = mu / h
         self.field_manger = FluxBiasController(field_range=(f_min, f_max))
 
     def time(self) -> float:
         """Calculate optimum measurements time"""
-        # TODO Переписать с использованием функции счетчика числа пиков между f_min и f_max
-        def t(n:int):
-            if n<0 and n is not isinstance(int):
+        def t(n: int) -> float:
+            """Return time ensures that the n peak falls at the beginning of the interval [f_min, f_max]"""
+            if n < 0 and t:
                 raise ValueError("n should be positive integer")
-            return pi * (1 + 2 * n) / self.m / self.f_min
+            return pi * (1 + 2 * n) / self.m / self.f_max
 
-        if self.f_min != 0:
-            n = 0
-            time_limit_counter = 0
-            while True:
-                time_limit_counter += 1
-                if (self.f_min + pi / t(n) / self.m) < self.f_max:  # Next Cos max is lefter than f_max
-                    return t(n - 1)
-                n += 1
-                if time_limit_counter >= 10 ** 5:
-                    raise ValueError("Unable to find optimal time")
-        if self.f_min == 0:
-            return np.pi / self.f_max
+        def peak(n, t) -> float:
+            """Calculate position (F = ...) of n peak of cos^2 ( m * F * T)"""
+            return 2 * pi * (n + 1) / self.m / t
+
+        for n in range(10 ** 9):
+            if peak(n-1, t(n)) > self.f_min:
+                return t(n - 1) # TODO Действия в случае если n = 0
 
     def do_measurements(self) -> dict:
         measurements = []
         for k in range(self.n):
-            q = Qubit(a=1 + 0 * i, b=0 + 0 * i)  #
+            q = Qubit(a=1 + 0 * i, b=0 + 0 * i)
             q.apply_gate(adamar_gate)
 
             time = self.time()
@@ -62,10 +56,8 @@ class Kitaev(Algorith):
             measurements = self.do_measurements()
             most_popular_state = max(measurements, key=measurements.get)  # | 0 > or | 1 >
             if most_popular_state == 0:
-                self.f_max = (self.f_max + self.f_min)/2
+                self.f_max = (self.f_max + self.f_min) / 2
             else:
                 self.f_min = (self.f_max + self.f_min) / 2
 
         return self.f_min, self.f_max
-
-
