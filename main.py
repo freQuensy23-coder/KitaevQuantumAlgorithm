158095fae3d1cc64aaa463bc0a54e293563a0e8f
frequensy <mametevalex@gmail.com>
2022-02-28 21:54:00 +0300

    Count goods and bads for each algorythm

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from algorithms import Kitaev
+from tqdm import tqdm
 
-f_min = 10
+f_min = 0
 f_max = 100
-N = 1000
-
+N = 10000
 
 if __name__ == "__main__":
-    algorithm = Kitaev(field_range=(f_min, f_max))
-    print(algorithm.work())
-    print(algorithm.field_manger.field)
-
+    g = 0
+    b = 0
+    for i in tqdm(range(1000)):
+        algorithm = Kitaev(field_range=(f_min, f_max))
+        l, r = algorithm.work()
+        f = algorithm.field_manger.field
+        if l <= f <= r:
+            g += 1
+        else:
+            b += 1
+            print(l, round(f, 2), r, "------")
+        # print(l, round(f, 2), r)
+    print(g, b)
