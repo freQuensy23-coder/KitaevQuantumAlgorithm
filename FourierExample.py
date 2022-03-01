d87933a5cb099ac9a28920299636d55c4ccb10dc
frequensy <mametevalex@gmail.com>
2022-03-01 12:42:11 +0300

    Example of usage Fourier alg

--- FourierExample.py
+++ FourierExample.py
@@ -3,8 +3,8 @@ from flux_bias import FluxBiasController
 
 field_range = (f_min, f_max) = (75, 100)
 
-alg = Fourier(field_range=field_range)
-alg.draw_truth().show()
-alg.work()
-alg.draw_truth().show()
+alg = Fourier(field_range=field_range, measurements_iterations=1)
+for i in range(15):
+    alg.draw_truth().show()
+    alg.work()
 print(alg.field_manger.field)
