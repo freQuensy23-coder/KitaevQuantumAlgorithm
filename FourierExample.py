db7504b611270ccee943fd49bd54467fb35a1a0c
frequensy <mametevalex@gmail.com>
2022-03-02 20:58:47 +0300

    Update tests

--- FourierExample.py
+++ FourierExample.py
@@ -1,10 +1,25 @@
-from algorithms import Fourier
-from flux_bias import FluxBiasController
+import numpy as np
+from matplotlib import pyplot as plt
+
+from algorithms import KitaevTruthScaling, Kitaev
 
 field_range = (f_min, f_max) = (75, 100)
+N = 10
 
-alg = Fourier(field_range=field_range, measurements_iterations=1)
-for i in range(15):
+alg = Fourier(field_range=field_range, measurements_iterations=1) # TODO убрать measurments iterations в метод work
+dfs = np.zeros(N)
+times = np.zeros(N)
+for i in range(N):
     alg.draw_truth().show()
     alg.work()
+    dfs[i] = alg.f_max - alg.f_min
+    times[i] = alg.field_manger.total_time
 print(alg.field_manger.field)
+plt.plot(np.log(times), np.log(dfs))
+plt.plot(np.log(times), np.log(dfs))
+plt.xlabel("log(t)")
+plt.ylabel("log($\\Delta$ F)")
+
+plt.plot(np.log(times), np.log(1/times))
+
+plt.show()
